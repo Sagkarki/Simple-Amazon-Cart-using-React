86547b877ec7e114e6d37836595df893fb57cbf6
Sagun Karki <sagunkarki111@gmail.com>
2021-06-05 15:45:07 -0400

    ADD: CartItems from data.js

--- amazon-cart/src/App.js
+++ amazon-cart/src/App.js
@@ -1,16 +1,22 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import CartItems from './Components/CartItems';
 import CartTotal from './Components/CartTotal';
+import data from'./Data'
 
 function App() {
  
+  const [cartItems, setCartItems]=useState(data);
+  
   return (
     <div className = "App">
      <Header title= "Amazon Cart"/>
      <div className="App-main">
-       <CartItems/>
+
+    
+       <CartItems items = {cartItems}/>
        <CartTotal/>
      </div>
     </div>
--- amazon-cart/src/Components/CartItems.js
+++ amazon-cart/src/Components/CartItems.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './CartItems.css'
 import CartItem from './CartItem'
-function CartItems() {
+
+function CartItems({items}) {
+    console.log("Inside CartItems component", items);
     return (
         <div className="CartItems">
             <h1>Shopping Cart</h1>
