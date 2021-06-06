7453ab9d19766a5d5e671c52245f2bd891bc8959
Sagun Karki <sagunkarki111@gmail.com>
2021-06-06 00:43:54 -0400

    ADD: Images in CartItems

--- amazon-cart/src/Components/CartItem.js
+++ amazon-cart/src/Components/CartItem.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import './CartItem.css'
 
-function CartItem() {
+function CartItem( {item}) {
     return (
         <div className="CartItem">
             <div className="CartItem-image">
-                <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-pro-12-11-select-202104_GEO_IN_FMT_WHH?wid=2000&hei=2000&fmt=jpeg&qlt=80&.v=1617865122000" alt=""/>
+            <img src={process.env.PUBLIC_URL + '/items/' + item.image} alt=""/>
             </div>
             <div class="CartItem-info">
                 <div className="info-title">
-                    <h2>Apple iPad Pro</h2>
+                    <h2>{item.title}</h2>
                 </div>
                 <div className="info-stock">
-                    In Stock
+                    {item.stock}
                 </div>
                 <div className="item-actions">
                     <div className="item-quantity">
-                        <select>
+                        <select value={item.quantity}>
                   \          <option value="1">Qty: 1</option>
                             <option value="2">Qty: 2</option>
                             <option value="3">Qty: 3</option>
@@ -30,7 +30,7 @@ function CartItem() {
                 </div>
             </div>
             <div className="CartItem-price">
-                $769.06
+                ${item.price}
             </div>
         </div>
     )
