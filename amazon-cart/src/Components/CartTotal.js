476b98a1c8a24cc8c92aeb502b6e2c01f1b541d6
Sagun Karki <sagunkarki111@gmail.com>
2021-06-06 01:10:54 -0400

    ADD: style to CartTotal

--- amazon-cart/src/Components/CartTotal.js
+++ amazon-cart/src/Components/CartTotal.js
@@ -5,7 +5,17 @@ import  './CartTotal.css'
 function CartTotal() {
     return (
         
-        <div className="CartTotal"></div>    
+        <div className="CartTotal">
+            <h3>
+                Subtotal (5 items):
+                <span className= "CartTotal-price">
+                    $2,172.38
+                </span>
+            </h3>
+            <button>
+                Proceed to checkout
+            </button>
+        </div>    
       
     )
 }
